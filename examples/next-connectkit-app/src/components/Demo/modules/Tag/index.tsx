e2c01dc126a9b0d5a3f03fa78e473901834ba0e8
tuguilin <tuguilin@minijoy.work>
2024-10-11 21:57:01 +0800

    fix: delete demo

--- examples/next-connectkit-app/src/components/Demo/modules/Tag/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.css';
-
-export default function Tag(props: { children: React.ReactNode; [key: string]: any }) {
-  return (
-    <div className={styles.tag} {...props}>
-      {props.children}
-    </div>
-  );
-}
